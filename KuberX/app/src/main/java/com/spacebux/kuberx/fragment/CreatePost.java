2b96de1520039edb454b1aa0b78f550232366254
Arpit <ajha3112@gmail.com>
2021-08-08 01:38:44 +0530

    Back Button Working

--- KuberX/app/src/main/java/com/spacebux/kuberx/fragment/CreatePost.java
+++ KuberX/app/src/main/java/com/spacebux/kuberx/fragment/CreatePost.java
@@ -3,19 +3,28 @@ package com.spacebux.kuberx.fragment;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.spacebux.kuberx.App;
 import com.spacebux.kuberx.R;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Objects;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +32,10 @@ import java.util.Objects;
  * create an instance of this fragment.
  */
 public class CreatePost extends Fragment {
+
+    private Button post;
+    private ImageView backBtn;
+    private NavController navController;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -62,14 +75,32 @@ public class CreatePost extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_post, container, false);
+        View view = inflater.inflate(R.layout.fragment_create_post, container, false);
+        setReferences(view);
+        configure();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+    }
+
+    private void configure(){
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_createPost_to_posts);
+            }
+        });
     }
 
     @SuppressLint("RestrictedApi")
@@ -97,4 +128,10 @@ public class CreatePost extends Fragment {
         floatingActionButton.setVisibility(View.VISIBLE);
 
     }
+
+    private void setReferences(View view) {
+        backBtn = view.findViewById(R.id.post_back_btn);
+    }
+
+
 }
